684135b05a00cecd2f6219abcf143c74856371fd
Amy Tu <siamesebb@gmail.com>
2018-05-22 17:50:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even? 
-    return "X"
-  else 
+  if turn_count(board).odd? 
     return "O"
+  else 
+    return "X"
   end
 end
   
